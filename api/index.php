7a0892933e71f380f50465380a17d9ca1356686f
MARYAM-HIBA-CAR <ibnatoomiha2008@gmail.com>
2026-09-19 21:29:01 +0100

    c

--- api/index.php
+++ api/index.php
@@ -558,7 +558,7 @@
             <div class="col-md-6">
 
                 <div class="module-card">
-                    <a href="/api/module/m201.php"  id="lien">
+                    <a href="/module/m201.php"  id="lien">
 
                     <div class="module-icon">
                         <i class="fa-brands fa-html5"></i>
